a41402e2918d103a9f5c24dcd643b19c02baa7dc
johhny11 <160652151+johhny11@users.noreply.github.com>
2025-09-09 14:38:53 +0300

    set up done

---
+++ ellatech/app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { Image } from "expo-image";
+import { Platform, StyleSheet } from "react-native";
+
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Text, View } from "react-native";
+import "../../global.css";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
